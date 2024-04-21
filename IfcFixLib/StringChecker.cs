a13f2906a9d64bbedee8945c0efe163b097985ba
Anatolii Ivanov <gotnickname@gmail.com>
2024-04-21 15:03:37 +0400

    changed StringChecker

--- IfcFixLib/StringChecker.cs
+++ IfcFixLib/StringChecker.cs
@@ -6,7 +6,7 @@ public class StringChecker
 {
     public StringFilterType FilterType
     {
-        get { return _nameFilterType; }
+        get { return _stringFilterType; }
         set
         {
             switch (value)
@@ -26,52 +26,48 @@ public class StringChecker
                 default:
                     throw new NotImplementedException();
             }
-            _nameFilterType = value;
+            _stringFilterType = value;
         }
     }
-    private StringFilterType _nameFilterType;
-    private delegate bool FilterNameCheckerDelegate(string name, string searchString);
+    private StringFilterType _stringFilterType;
+    private delegate bool FilterNameCheckerDelegate(string prop, string searchString);
     private FilterNameCheckerDelegate CheckDelegate;
     private string? _containsCasheValue = null;
     private string[]? _containsCashe = null;
     private Dictionary<string, Regex> _regexCache = new Dictionary<string, Regex>();
 
-    public bool Check(string name, string searchString)
+    public bool Check(string prop, string searchString)
     {
-        return CheckDelegate(name, searchString);
+        return CheckDelegate(prop, searchString);
     }
 
-    private bool EqualsCheck(string name, string searchString)
+    private bool EqualsCheck(string prop, string searchString)
     {
-        return name == searchString;
+        return prop == searchString;
     }
 
-    private bool ContainsCheck(string name, string searchString)
+    private bool ContainsCheck(string prop, string searchString)
     {
-        return name.Contains(searchString);
+        return prop.Contains(searchString);
     }
-    private bool ContainsAnyCheck(string name, string searchString)
+    private bool ContainsAnyCheck(string prop, string searchString)
     {
         if ((searchString != _containsCasheValue))
         {
             _containsCasheValue = searchString;
             _containsCashe = searchString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
         }
-        // Split the search string into words
-        
-        // Check if any of the words from the search string are present in the name
         foreach (string word in _containsCashe)
         {
-            if (name.Contains(word))
+            if (prop.Contains(word))
             {
                 return true;
             }
         }
-        
         return false;
     }
 
-    private bool CheckRegex(string name, string searchString)
+    private bool CheckRegex(string prop, string searchString)
     {
         if (!_regexCache.TryGetValue(searchString, out Regex regex))
         {
@@ -79,6 +75,6 @@ public class StringChecker
             _regexCache[searchString] = regex;
         }
 
-        return regex.IsMatch(name);
+        return regex.IsMatch(prop);
     }
 }
