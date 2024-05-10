d167b34382ae664e7075cda5c05170ef4d20ab26
Anatolii Ivanov <gotnickname@gmail.com>
2024-05-10 20:42:27 +0400

    dynamic components render out from models list

---
+++ WebIfcFix/Shared/ChildComponentsModels.cs
@@ -0,0 +1,19 @@
+﻿using IfcFixLib;
+
+namespace WebIfcFix.Shared;
+
+public interface IChildComponentModel
+{
+    Type ComponentType { get;} 
+    Dictionary<string, object> Params => new() { ["Model"] = this };
+}
+public class ChildComponent1Model : IChildComponentModel
+{
+    public string Input { get; set; } = String.Empty;
+    public Type ComponentType { get; private set; } = typeof(ChildComponent1);
+}
+public class ChildComponent2Model : IChildComponentModel
+{
+    public ElementStringValueType StringValueType { get; set; } = ElementStringValueType.Name;
+    public Type ComponentType { get; private set; } = typeof(ChildComponent2);
+}
