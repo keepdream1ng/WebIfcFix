0efe0a7a5e2b350ed97cd098b8c1f9fcbd8d27e5
Anatolii Ivanov <gotnickname@gmail.com>
2024-05-11 10:41:51 +0400

    components state gets passed by a query

--- WebIfcFix/Shared/ChildComponentsModels.cs
+++ WebIfcFix/Shared/ChildComponentsModels.cs
@@ -1,4 +1,6 @@
 ﻿using IfcFixLib;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WebIfcFix.Shared;
 
@@ -7,20 +9,67 @@ public interface IChildComponentModelBase
     string? ComponentTypeNane { get;}
     Dictionary<string, object> Params => new() { ["Model"] = this };
 }
-public abstract class ChildComponentModelBase
+public class ChildComponentModelBase
 {
-    public virtual string? ComponentTypeNane { get;}
-    public virtual Dictionary<string, object> Params { get; private set; } 
+    public virtual string? ComponentTypeNane { get; }
+    public virtual string? ModelDerivedType { get; set; }
+    public virtual Dictionary<string, object> Params() => new() { ["Model"] = this };
 }
-public class ChildComponent1Model : ChildComponentModelBase, IChildComponentModelBase
+
+public class ChildComponentModelDerived : ChildComponentModelBase
+{
+    public override string? ModelDerivedType  => this.GetType().FullName;
+}
+
+public class ChildComponent1Model : ChildComponentModelDerived 
 {
     public string Input { get; set; } = String.Empty;
+
+    [JsonIgnore]
     public override string? ComponentTypeNane => typeof(ChildComponent1).FullName;
-    public override Dictionary<string, object> Params => new() { ["Model"] = this };
 }
-public class ChildComponent2Model : ChildComponentModelBase, IChildComponentModelBase
+public class ChildComponent2Model : ChildComponentModelDerived 
 {
     public ElementStringValueType StringValueType { get; set; } = ElementStringValueType.Name;
+
+    [JsonIgnore]
     public override string? ComponentTypeNane => typeof(ChildComponent2).FullName;
-    public override Dictionary<string, object> Params => new() { ["Model"] = this };
+}
+public class ChildComponentModelConverter : JsonConverter<ChildComponentModelBase>
+{
+    public override bool CanRead => true;
+    public override bool CanWrite => false;
+
+    public override ChildComponentModelBase ReadJson(JsonReader reader, Type objectType, ChildComponentModelBase existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        JObject jsonObject = JObject.Load(reader);
+        string? modelType = jsonObject.GetValue(nameof(ChildComponentModelBase.ModelDerivedType))?.ToString();
+        if (modelType is null)
+        {
+            throw new JsonSerializationException($"Could not deserialize");
+        }
+        
+        // Determine the concrete type to create based on the ModelDerivedType property
+        ChildComponentModelBase? component;
+        var type = Type.GetType(modelType);
+        if (type is null)
+        {
+            throw new JsonSerializationException($"Unknown component type: {modelType}");
+        }
+
+        component = Activator.CreateInstance( type! ) as ChildComponentModelBase;
+        if (component is null)
+        {
+            throw new JsonSerializationException($"Error on casting to {nameof(ChildComponentModelBase)}");
+        }
+
+        // Populate the properties of the concrete type
+        serializer.Populate(jsonObject.CreateReader(), component);
+        return component;
+    }
+
+    public override void WriteJson(JsonWriter writer, ChildComponentModelBase value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
 }
