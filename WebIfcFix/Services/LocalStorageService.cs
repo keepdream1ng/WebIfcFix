64c71a4f3f93097e481762b2798f8644edcb2361
Anatolii Ivanov <gotnickname@gmail.com>
2024-05-12 15:45:08 +0400

    added LocalStorageService

---
+++ WebIfcFix/Services/LocalStorageService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.JSInterop;
+using Newtonsoft.Json;
+
+namespace WebIfcFix.Services;
+public interface ILocalStorageService
+{
+    Task SetItemAsync<T>(string key, T value);
+    Task<T?> GetItemAsync<T>(string key);
+    Task RemoveItemAsync(string key);
+}
+public class LocalStorageService(
+    IJSRuntime _jsRuntime,
+    IJsonConvertService _json
+    ): ILocalStorageService
+{
+    public async Task SetItemAsync<T>(string key, T value)
+    {
+        await _jsRuntime.InvokeVoidAsync("localStorage.setItem", key, _json.SerializeObject(value));
+    }
+
+    public async Task<T?> GetItemAsync<T>(string key)
+    {
+        string storedStrValue = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", key);
+        if (String.IsNullOrEmpty(storedStrValue))
+        {
+            return default;
+        }
+        else
+        {
+            return _json.DeserializeObject<T>(storedStrValue);
+        }
+    }
+
+    public async Task RemoveItemAsync(string key)
+    {
+        await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", key);
+    }
+}
