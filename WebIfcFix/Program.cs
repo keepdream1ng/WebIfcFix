3c2c376e1646d3ce367b34ad8d53d7379a843388
Anatolii Ivanov <gotnickname@gmail.com>
2024-05-12 14:03:36 +0400

    moved serialization and deserialization logic in JsonConvertService

--- WebIfcFix/Program.cs
+++ WebIfcFix/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebIfcFix;
+using WebIfcFix.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IJsonConvertService, JsonConvertService>();
 
 await builder.Build().RunAsync();
---
+++ WebIfcFix/Services/JsonConvertService.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebIfcFix.Shared;
+
+namespace WebIfcFix.Services;
+public interface IJsonConvertService
+{
+	T? DeserializeObject<T>(string json);
+	string SerializeObject(object? obj);
+}
+
+public class JsonConvertService : IJsonConvertService
+{
+	public string SerializeObject(object? obj)
+	{
+		return JsonConvert.SerializeObject(obj, Formatting.Indented);
+	}
+
+	public T? DeserializeObject<T>(string json)
+	{
+		var settings = new JsonSerializerSettings
+		{
+			Converters = { new ChildComponentModelConverter() }
+		};
+		return JsonConvert.DeserializeObject<T>(json, settings);
+	}
+
+	// Custom converters for this web app logic.
+	class ChildComponentModelConverter : JsonConverter<ChildComponentModelBase>
+	{
+		public override bool CanRead => true;
+		public override bool CanWrite => false;
+
+		public override ChildComponentModelBase ReadJson(JsonReader reader, Type objectType, ChildComponentModelBase existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			JObject jsonObject = JObject.Load(reader);
+			string? modelType = jsonObject.GetValue(nameof(ChildComponentModelBase.ModelDerivedType))?.ToString();
+			if (modelType is null)
+			{
+				throw new JsonSerializationException($"Could not deserialize");
+			}
+
+			// Determine the concrete type to create based on the ModelDerivedType property.
+			ChildComponentModelBase? component;
+			var type = Type.GetType(modelType);
+			if (type is null)
+			{
+				throw new JsonSerializationException($"Unknown component type: {modelType}");
+			}
+
+			component = Activator.CreateInstance(type!) as ChildComponentModelBase;
+			if (component is null)
+			{
+				throw new JsonSerializationException($"Error on casting to {nameof(ChildComponentModelBase)}");
+			}
+
+			// Populate the properties of the concrete type.
+			serializer.Populate(jsonObject.CreateReader(), component);
+			return component;
+		}
+
+		public override void WriteJson(JsonWriter writer, ChildComponentModelBase value, JsonSerializer serializer)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
