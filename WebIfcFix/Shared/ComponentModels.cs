9b9b187e8debf9fbe357a31dc98f4bf2f499acc1
Anatolii Ivanov <gotnickname@gmail.com>
2024-05-19 15:59:47 +0400

    added base component with model type

--- WebIfcFix/Shared/ComponentModels.cs
+++ WebIfcFix/Shared/ComponentModels.cs
@@ -7,7 +7,7 @@ public abstract class SerializableModelBase
 {
     public virtual string? ModelType { get; }
     public virtual string? ComponentTypeName { get; }
-    public virtual Dictionary<string, object> Params() => new() { ["Model"] = this };
+    public virtual Dictionary<string, object> Params() => new() { ["ModelBase"] = this };
 }
 
 public abstract class ComponentModel<T> : SerializableModelBase where T : IComponent
@@ -17,4 +17,23 @@ public abstract class ComponentModel<T> : SerializableModelBase where T : ICompo
     // This property doesnt need to be serialized, json converter vill create derived class based on ModelType property.
     [JsonIgnore]
     public override string? ComponentTypeName => typeof(T).FullName;
+}
+
+public abstract class DynamicComponentWithModelBase<Tmodel> : ComponentBase, IComponent where Tmodel : SerializableModelBase
+{
+    private Tmodel? _model;
+
+	[Parameter]
+	[EditorRequired]
+	public SerializableModelBase ModelBase
+    {
+        get => _model!;
+        set { _model = value as Tmodel; }
+    }
+
+    public virtual Tmodel Model 
+    {
+        get => _model!;
+        set { _model = value; }
+    }
 }
