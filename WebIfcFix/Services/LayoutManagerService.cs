854037cf225ce3103fc2998008f704b33b0bb151
Anatolii Ivanov <gotnickname@gmail.com>
2024-06-16 12:57:15 +0400

    updated LayoutManagerService

--- WebIfcFix/Services/LayoutManagerService.cs
+++ WebIfcFix/Services/LayoutManagerService.cs
@@ -4,33 +4,20 @@ using WebIfcFix.Shared;
 
 namespace WebIfcFix.Services;
 
-public class LayoutManagerService
+public class LayoutManagerService(IComponentsTypesService componentsTypes)
 {
     public DbParser DbParser { get; private set; } = new();
     public DbSerializer DbSerializer { get; private set; } = new();
     public PipelineManager? PipelineManager { get; private set; }
-    public List<SerializableModelBase> ComponentsLayout { get; private set; } = new();
+    public IReadOnlyList<SerializableModelBase> ComponentsLayout => _componentsLayout.AsReadOnly();
+    private List<SerializableModelBase> _componentsLayout = new();
+    private IComponentsTypesService _typesService = componentsTypes;
 
     public void ReorderLayout((int oldIndex, int newIndex) indexes)
     {
-		var itemToMove = ComponentsLayout[indexes.oldIndex];
-		ComponentsLayout.RemoveAt(indexes.oldIndex);
-        PipelineManager!.Remove(itemToMove.PipelineNode!);
-
-		if (indexes.newIndex < ComponentsLayout.Count)
-		{
-            var nextComponent = ComponentsLayout[indexes.newIndex + 1];
-            itemToMove.PipelineNode = PipelineManager.
-                AddToPipeline(itemToMove.PipeFilter, beforeNode: nextComponent.PipelineNode);
-			ComponentsLayout.Insert(indexes.newIndex, itemToMove);
-		}
-		else
-		{
-            DbSerializer.UnsubscribeFrom(PipelineManager.PipeEnd);
-            itemToMove.PipelineNode = PipelineManager.AddToPipeline(itemToMove.PipeFilter);
-            DbSerializer.SubscribeToOutput(PipelineManager.PipeEnd);
-			ComponentsLayout.Add(itemToMove);
-		}
+		var itemToMove = _componentsLayout[indexes.oldIndex];
+        RemoveElementAt(indexes.oldIndex);
+        InsertAtIndex(itemToMove, indexes.newIndex);
     }
 
     public void ImportLayout(List<SerializableModelBase> components)
@@ -41,9 +28,43 @@ public class LayoutManagerService
             DbSerializer.UnsubscribeFrom(PipelineManager.PipeEnd);
 			PipelineManager.Dispose();
         }
-        ComponentsLayout = components;
+        _componentsLayout = components;
         PipelineManager = new PipelineManager(DbParser);
-        ComponentsLayout.ForEach(c => PipelineManager.AddToPipeline(c.PipeFilter));
+        _componentsLayout.ForEach(c => c.PipelineNode = PipelineManager.AddToPipeline(c.PipeFilter));
         DbSerializer.SubscribeToOutput(PipelineManager.PipeEnd);
+    }
+
+    public void RemoveElementAt(int index)
+    {
+        if (PipelineManager is null || _componentsLayout.Count <= index) return;
+        PipelineManager!.StopProcessing();
+        PipelineManager!.Remove(_componentsLayout[index].PipelineNode!);
+		_componentsLayout.RemoveAt(index);
+    }
+
+    public void InsertNew((int oldIndex, int newIndex) indexes)
+    {
+		IComponentInformation pickedTypeInfo = _typesService.ComponentsTypes[indexes.oldIndex];
+		SerializableModelBase? pickedModel = Activator.CreateInstance(pickedTypeInfo.ModelType) as SerializableModelBase;
+        if (pickedModel is null) return;
+        InsertAtIndex(pickedModel, indexes.newIndex);
+    }
+
+    private void InsertAtIndex(SerializableModelBase item, int index)
+    {
+		if (index < _componentsLayout.Count)
+		{
+			SerializableModelBase nextComponent = _componentsLayout[index + 1];
+            item.PipelineNode = PipelineManager!
+                .AddToPipeline(item.PipeFilter, beforeNode: nextComponent.PipelineNode);
+			_componentsLayout.Insert(index, item);
+		}
+		else
+		{
+			DbSerializer.UnsubscribeFrom(PipelineManager!.PipeEnd);
+			item.PipelineNode = PipelineManager.AddToPipeline(item.PipeFilter);
+			DbSerializer.SubscribeToOutput(PipelineManager.PipeEnd);
+			_componentsLayout.Add(item);
+		}
     }
 }
