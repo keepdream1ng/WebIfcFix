4420cbdf0850d402609691791da07d1d4aceed48
keepdream1ng <106347436+keepdream1ng@users.noreply.github.com>
2024-07-02 03:34:42 +0000

    Deploying to gh-pages from @ keepdream1ng/WebIfcFix@fe53dc6f314da1edac1be0b510b2f634c2966b5e 🚀

--- scripts.js
+++ scripts.js
@@ -6,3 +6,10 @@ function saveAsFile(filename, bytesBase64) {
     link.click();
     document.body.removeChild(link);
 }
+function copyTextToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Text copied to clipboard');
+    }).catch(function(error) {
+        console.error('Could not copy text: ', error);
+    });
+}
