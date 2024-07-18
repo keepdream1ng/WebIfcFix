5e5d67e5f1c01c6d50bb36844a1e91de145d9c53
Anatolii Ivanov <gotnickname@gmail.com>
2024-07-18 21:06:26 +0400

    now UrlQueryService commpresses layout for url

--- WebIfcFix/Program.cs
+++ WebIfcFix/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSingleton<IJsonConvertService, JsonConvertService>();
 builder.Services.AddSingleton<ILocalStorageService, LocalStorageService>();
 builder.Services.AddSingleton<IComponentsTypesService, ComponentsTypesService>();
 builder.Services.AddSingleton<ISanitizerService, SanitizerService>();
+builder.Services.AddSingleton<IUrlQueryParameterService, UrlQueryService>();
 builder.Services.AddSingleton<LayoutManagerService>();
 
 await builder.Build().RunAsync();
---
+++ WebIfcFix/Services/UrlQueryService.cs
@@ -0,0 +1,50 @@
+﻿using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace WebIfcFix.Services;
+
+public interface IUrlQueryParameterService
+{
+	string GetQueryParameterFromJson(string jsonLayout);
+	string GetSanitizedJsonFromQueryParameter(string unsaveStringParameter);
+}
+public class UrlQueryService(ISanitizerService Sanitizer) : IUrlQueryParameterService
+{
+	public string GetQueryParameterFromJson(string jsonLayout)
+	{
+		byte[] data = Encoding.UTF8.GetBytes(jsonLayout);
+		byte[] compressedData;
+
+		using (var memoryStream = new MemoryStream())
+		{
+			using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress))
+			{
+				gzipStream.Write(data, 0, data.Length);
+			}
+			compressedData = memoryStream.ToArray();
+		}
+		string compressedString = Convert.ToBase64String(compressedData);	
+		string queryParameter = WebUtility.UrlEncode(compressedString);
+		return queryParameter;
+	}
+
+	public string GetSanitizedJsonFromQueryParameter(string unsaveStringParameter)
+	{
+		string jsonCompressed = WebUtility.UrlDecode(unsaveStringParameter);
+		byte[] compressedData = Convert.FromBase64String(jsonCompressed);
+		byte[] decompressedData;
+
+		using (var compressedStream = new MemoryStream(compressedData))
+		using (var gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+		using (var resultStream = new MemoryStream())
+		{
+			gzipStream.CopyTo(resultStream);
+			decompressedData = resultStream.ToArray();
+		}
+
+		string decompressedJson = Encoding.UTF8.GetString(decompressedData);
+		string sanitazedJson = Sanitizer.SanitizeJson(decompressedJson);
+		return sanitazedJson;
+	}
+}
