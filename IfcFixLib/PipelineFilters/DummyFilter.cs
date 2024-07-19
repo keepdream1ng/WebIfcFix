19dc83f069302c5435eddbd9a42b2798e9d28a8f
Anatolii Ivanov <gotnickname@gmail.com>
2024-07-19 16:11:51 +0400

    added broken link handling

---
+++ IfcFixLib/PipelineFilters/DummyFilter.cs
@@ -0,0 +1,10 @@
+﻿using IfcFixLib.IfcPipelineDefinition;
+
+namespace IfcFixLib.PipelineFilters;
+public class DummyFilter : PipeFilter
+{
+	protected override Task<DataIFC> ProcessDataAsync(DataIFC dataIFC, CancellationToken cancellationToken)
+	{
+		return Task.FromResult(dataIFC);
+	}
+}
---
+++ WebIfcFix/Filters/BrokenLinkIndicator.razor.cs
@@ -0,0 +1,13 @@
+﻿using IfcFixLib.IfcPipelineDefinition;
+using IfcFixLib.PipelineFilters;
+using WebIfcFix.Shared;
+
+namespace WebIfcFix.Filters;
+
+public class BrokenLinkIndicatorModel : ComponentModel<BrokenLinkIndicator>
+{
+	public override IPipeFilter PipeFilter => _dummyFilter;
+	private DummyFilter _dummyFilter = new();
+
+	public override IComponentInformation ComponentInformation { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+}
