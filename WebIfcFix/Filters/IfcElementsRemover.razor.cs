2e646275a0d075ec5c56d7c99d71745237d921be
Anatolii Ivanov <gotnickname@gmail.com>
2024-08-25 14:56:06 +0400

    added IfcElementsRemover component

---
+++ WebIfcFix/Filters/IfcElementsRemover.razor.cs
@@ -0,0 +1,20 @@
+﻿using IfcFixLib.IfcPipelineDefinition;
+using IfcFixLib.PipelineFilters;
+using WebIfcFix.Shared;
+
+namespace WebIfcFix.Filters;
+
+public class IfcElementsRemoverModel : ComponentModel<IfcElementsRemover>
+{
+	public override IPipeFilter PipeFilter => _remover;
+    private ElementsRemover _remover = new ElementsRemover();
+
+	public override IComponentInformation ComponentInformation { get; init; } = new IfcElementsRemoverInfo();
+}
+
+public class IfcElementsRemoverInfo : ComponentInformation<IfcElementsRemoverModel>
+{
+    public override string FilterName => "Remove from model";
+
+    public override string FilterInstructions => "This component will remove filtered elements from ifc model (database) by creating new model and copying everything but those elements. Next filter component will be dealing with new model instead and rest of the elements.";
+}
