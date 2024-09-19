fd5079df7ae1f95a74bfe561139847ab5b9d6667
Anatolii Ivanov <gotnickname@gmail.com>
2024-09-19 15:42:02 +0400

    added PropertyEditor component

---
+++ WebIfcFix/Filters/PropertyEditor.razor.cs
@@ -0,0 +1,29 @@
+﻿using IfcFixLib;
+using IfcFixLib.IfcPipelineDefinition;
+using IfcFixLib.PipelineFilters;
+using WebIfcFix.Shared;
+
+namespace WebIfcFix.Filters;
+
+public class PropertyEditorModel : ComponentModel<PropertyEditor>
+{
+	public override IPipeFilter PipeFilter => _valueSetter;
+	public StringValueSetter ValueSetterStrategy { get; set; }
+
+	public override IComponentInformation ComponentInformation { get; init;} = new PropertyEditorInfo();
+
+	private ValueSetter _valueSetter;
+
+    public PropertyEditorModel()
+    {
+		ValueSetterStrategy = new StringValueSetter(); 
+		_valueSetter = new ValueSetter(ValueSetterStrategy);
+    }
+}
+
+public class PropertyEditorInfo : ComponentInformation<PropertyEditorModel>
+{
+	public override string FilterName => "Edit property";
+
+	public override string FilterInstructions => "Edit properties of the filtered previously elements.";
+}
