f811b68caf8d222d28e5b1d01dbde2507a77d0f8
Anatolii Ivanov <gotnickname@gmail.com>
2024-09-28 14:44:03 +0400

    added Property Copier pipeline filter

---
+++ WebIfcFix/Filters/PropertyCopier.razor.cs
@@ -0,0 +1,32 @@
+﻿using IfcFixLib;
+using IfcFixLib.FilterStrategy;
+using IfcFixLib.IfcPipelineDefinition;
+using IfcFixLib.PipelineFilters;
+using WebIfcFix.Shared;
+
+namespace WebIfcFix.Filters;
+
+public class PropertyCopierModel : ComponentModel<PropertyCopier>
+{
+	public override IPipeFilter PipeFilter => _valueCopier;
+	public StringValueSetter ValueSetterStrategy { get; set; }
+	public StringValueGetter ValueGetterStrategy { get; set; }
+
+	public override IComponentInformation ComponentInformation { get; init;} = new PropertyCopierInfo();
+
+	private ValueCopier _valueCopier;
+
+    public PropertyCopierModel()
+    {
+		ValueGetterStrategy = new StringValueGetter();
+		ValueSetterStrategy = new StringValueSetter(); 
+		_valueCopier = new ValueCopier(ValueGetterStrategy, ValueSetterStrategy);
+    }
+}
+
+public class PropertyCopierInfo : ComponentInformation<PropertyCopierModel>
+{
+	public override string FilterName => "Copy property";
+
+	public override string FilterInstructions => "Copy property value and assign it to another property inside of the element.";
+}
