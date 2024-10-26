60c8f9f3a0a8f923e6000ac8cee613b02a32d96a
Anatolii Ivanov <gotnickname@gmail.com>
2024-10-26 18:17:00 +0400

    load predefined layouts from server feature

--- WebIfcFix/Program.cs
+++ WebIfcFix/Program.cs
@@ -8,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<PredefinedLayoutsService>();
 builder.Services.AddSingleton<IJsonConvertService, JsonConvertService>();
 builder.Services.AddSingleton<ILocalStorageService, LocalStorageService>();
 builder.Services.AddSingleton<IComponentsTypesService, ComponentsTypesService>();
---
+++ WebIfcFix/Services/PredefinedLayoutsService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+using WebIfcFix.Shared;
+
+namespace WebIfcFix.Services;
+
+public class PredefinedLayoutsService(HttpClient httpClient, IJsonConvertService convertService)
+{
+	private Dictionary<string, List<SerializableModelBase>>? _predefinedFilterComponentsLayouts;
+
+	public async Task LoadDataAsync()
+	{
+		string json = await httpClient.GetStringAsync("data/predefinedFilterComponentsLayouts.json");
+		_predefinedFilterComponentsLayouts = convertService.DeserializeObject<Dictionary<string, List<SerializableModelBase>>>(json);
+	}
+
+	public string[] GetPredefinedLayoutsNames()
+	{
+		string[] names;
+		if (_predefinedFilterComponentsLayouts is not null)
+		{
+			names = _predefinedFilterComponentsLayouts.Keys.ToArray();
+		}
+		else
+		{
+			names = Array.Empty<string>();
+		}
+		return names;
+	}
+
+	public List<SerializableModelBase>? GetLayoutByName(string name)
+	{
+		return _predefinedFilterComponentsLayouts?[name];
+	}
+}
