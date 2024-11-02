5e3eebe82c39569b7011363bad404da5f301c49c
Anatolii Ivanov <gotnickname@gmail.com>
2024-11-02 18:23:21 +0400

    added FilterByClassNameComponent

---
+++ WebIfcFix/Filters/FilterByClassNameComponent.razor.cs
@@ -0,0 +1,27 @@
+﻿using IfcFixLib.FilterStrategy;
+using IfcFixLib.IfcPipelineDefinition;
+using IfcFixLib.PipelineFilters;
+using WebIfcFix.Shared;
+
+namespace WebIfcFix.Filters;
+
+public class FilterByClassNameComponentModel : ComponentModel<FilterByClassNameComponent>
+{
+    public override IPipeFilter PipeFilter => _filter;
+    public FilterClassNameStrategy FilterStrategy { get; set; }
+    public override IComponentInformation ComponentInformation { get; init; }
+    private ElementsFilter _filter;
+    public FilterByClassNameComponentModel()
+    {
+        FilterStrategy = new FilterClassNameStrategy();
+        _filter = new ElementsFilter(FilterStrategy);
+        ComponentInformation = new FilterByClassNameComponentInfo();
+    }
+}
+
+public class FilterByClassNameComponentInfo : ComponentInformation<FilterByClassNameComponentModel>
+{
+    public override string FilterName => "Filter by IFC class";
+
+    public override string FilterInstructions => "Select the IFC class name for the elements you want to filter on and process in the next component.";
+}
